454d7fc3f48dcc68673fc9aa0446d20932f36217
guramijibladze <guramijibladze@gmail.com>
2021-02-22 13:35:40 +0400

    up

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-
 isHidden = HTMLElement.hidden;
 HTMLElement.hidden = true | false;
+
+
+document.getElementById('add-list').hidden = false;
+let x = 0;
+document.getElementById('add-list').addEventListener('click', function(){
+    x++;
+    if( x == 4){
+        document.getElementById('add-list').hidden = true;
+        return alert("სიაში შეგვიძლია მხოლო სამი ლისტის დამატება");
+    }else{
+        document.getElementById(x).hidden = false;
+    }
+})
